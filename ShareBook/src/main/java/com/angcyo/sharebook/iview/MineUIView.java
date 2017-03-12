344b9f04c05f410d4551bdf08c27ed01d465c1b8
angcyo <angcyo@126.com>
2017-03-12 14:19:05 +0800

    fix framework

--- ShareBook/src/main/java/com/angcyo/sharebook/iview/MineUIView.java
+++ ShareBook/src/main/java/com/angcyo/sharebook/iview/MineUIView.java
@@ -1,78 +1,76 @@
 package com.angcyo.sharebook.iview;
 
-import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.widget.RelativeLayout;
-
 import com.angcyo.sharebook.R;
-import com.angcyo.uiview.base.UIRecyclerUIView;
-import com.angcyo.uiview.recycler.RBaseItemDecoration;
+import com.angcyo.uiview.base.Item;
+import com.angcyo.uiview.base.SingleItem;
+import com.angcyo.uiview.base.UIItemUIView;
 import com.angcyo.uiview.recycler.RBaseViewHolder;
-import com.angcyo.uiview.recycler.RExBaseAdapter;
-import com.angcyo.uiview.recycler.widget.IShowState;
+import com.angcyo.uiview.widget.ItemInfoLayout;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by angcyo on 2017-03-11.
  */
 
-public class MineUIView extends UIRecyclerUIView {
+public class MineUIView extends UIItemUIView<SingleItem> {
 
     @Override
     protected String getTitleString() {
         return "我";
     }
 
-    @Override
-    protected RExBaseAdapter createAdapter() {
-        return new RExBaseAdapter<String, MainItemBean, String>(mActivity) {
-            @Override
-            protected int getItemLayoutId(int viewType) {
-                return R.layout.item_home_type_layout;
-            }
-
-            @Override
-            protected void onBindDataView(RBaseViewHolder holder, int posInData, MainItemBean dataBean) {
-                holder.tv(R.id.text_view).setText("pos:" + posInData);
-                holder.reV(R.id.recycler_view);
-            }
-        };
-    }
 
     @Override
-    protected RecyclerView.ItemDecoration createItemDecoration() {
-        return new RBaseItemDecoration(getResources().getDimensionPixelSize(R.dimen.base_xhdpi), getResources().getColor(R.color.default_base_line));
-    }
-
-    @Override
-    protected void afterInflateView(RelativeLayout baseContentLayout) {
-        mExBaseAdapter.setShowState(IShowState.LOADING);
-    }
-
-    @Override
-    public void onViewShowFirst(Bundle bundle) {
-        super.onViewShowFirst(bundle);
-        mExBaseAdapter.setShowState(IShowState.NORMAL);
-        mExBaseAdapter.resetAllData(createAllDatas());
-    }
-
-    @Override
-    public void onViewShow(Bundle bundle) {
-        super.onViewShow(bundle);
-        //test();
-    }
-
-    private List<MainItemBean> createAllDatas() {
-        List<MainItemBean> beanList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            beanList.add(new MainItemBean());
+    protected int getItemLayoutId(int viewType) {
+        if (viewType == 0) {
+            return R.layout.item_user_top_layout;
         }
-        return beanList;
+        return R.layout.item_info_layout;
     }
 
-    public static class MainItemBean {
-    }
+    @Override
+    protected void createItems(List<SingleItem> items) {
+        items.add(new SingleItem(SingleItem.Type.TOP) {
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
 
+            }
+        });
+        items.add(new SingleItem(SingleItem.Type.TOP) {
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
+                ItemInfoLayout itemLayout = holder.v(R.id.item_info_layout);
+                itemLayout.setItemText("测试条目:" + posInData);
+            }
+        });
+        items.add(new SingleItem(SingleItem.Type.TOP) {
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
+                ItemInfoLayout itemLayout = holder.v(R.id.item_info_layout);
+                itemLayout.setItemText("测试条目:" + posInData);
+            }
+        });
+        items.add(new SingleItem(SingleItem.Type.TOP) {
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
+                ItemInfoLayout itemLayout = holder.v(R.id.item_info_layout);
+                itemLayout.setItemText("测试条目:" + posInData);
+            }
+        });
+        items.add(new SingleItem(SingleItem.Type.LINE) {
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
+                ItemInfoLayout itemLayout = holder.v(R.id.item_info_layout);
+                itemLayout.setItemText("测试条目:" + posInData);
+            }
+        });
+        items.add(new SingleItem(SingleItem.Type.TOP_LINE) {
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
+                ItemInfoLayout itemLayout = holder.v(R.id.item_info_layout);
+                itemLayout.setItemText("测试条目:" + posInData);
+            }
+        });
+    }
 }
